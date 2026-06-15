9885310ad5954028ade2f81ed90c4487a1b56057
Philip Theuri <philtheuri@gmail.com>
2026-06-15 15:42:57 +0300

    do... while loops

--- type.c
+++ type.c
@@ -600,7 +600,42 @@ bool ageCheck(int age)
 {
     return age >= 19;
 }*/
+// while loops in c
+
 int main()
 {
+    /* int number = 0;
+     do
+     {
+         printf("Enter a number greater than 0:");
+         scanf("%d", &number);
+     } while (number <= 0);*/
+
+    /* char name[50] = "";
+     printf("Enter your name: ");
+     fgets(name, sizeof(name), stdin);
+     name[strcspn(name, "\n")] = '\0';
+
+     while (strlen(name) == 0)
+     {
+         printf("Name cannot be empty,please enter your name: ");
+         fgets(name, sizeof(name), stdin);
+         name[strcspn(name, "\n")] = '\0';
+     }
+     printf("Hello %s", name);*/
+
+    bool isRunning = true;
+    char response = '\0';
+    do
+    {
+        printf("You are playing a game!\n");
+        printf("Would you like to continoue?(Y = Yes, N = No):");
+        scanf(" %c", &response);
+        if (response != 'Y' && response != 'y')
+        {
+            isRunning = false;
+        }
+    } while (isRunning);
+    printf("You've exited the game!");
     return 0;
 }
