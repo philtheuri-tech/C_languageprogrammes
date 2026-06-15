e06d2491e7f919c6ca85b8ad51d6b2761d4b071b
Philip Theuri <philtheuri@gmail.com>
2026-06-15 22:07:17 +0300

    nested loops

--- type.c
+++ type.c
@@ -680,5 +680,24 @@ int main()
          }
          printf("\n");
      }*/
+
+    int rows = 0;
+    int columns = 0;
+    char symbol = '\0';
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+    printf("Enter the number of columns: ");
+    scanf("%d", &columns);
+    printf("Enter the symbol: ");
+    scanf(" %c", &symbol);
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int i = 0; i < columns; i++)
+        {
+            printf("%c", symbol);
+        }
+        printf("\n");
+    }
     return 0;
 }
